30d0c1e836a12528274826f1c9ff28513fc9ac10
Hamza Naseer <hamzanaseer711@gmail.com>
2024-09-11 22:04:35 +0500

    svgs setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Logo } from "@assets";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Logo />
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+// The import must start from a capital letter, since we'll directly use it as a react component
+import Logo from "./a.svg?react";
+export { Logo };
